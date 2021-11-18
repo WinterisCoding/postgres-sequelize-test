a4fdf0eab1e3df8e14e5c9f36e5a31c1cf8bcaf2
WinterisCoding <elisabethwinter.writes@gmail.com>
2021-11-17 21:09:20 -0500

    server response working

--- routes/enemies.js
+++ routes/enemies.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const router = express.Router();
+const db = require('../config/database')
+const Enemy = require('../models/Enemies')
 
-router.get('/', (req, res) => res.send('ENEMIES'));
+
+router.get('/', (req, res) => 
+    Enemy.findAll()
+    .then(enemies => {
+        console.log(enemies)
+        res.sendStatus(200);
+    })
+    .catch(err => console.log(err))
+);
 
 module.exports = router
